23c7556769250854424b9f9040ee0217598a268a
startuser0604 <vpdtn0604@gmail.com>
2025-09-23 18:02:59 +0900

    Update page.tsx
    최종

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
           src="/차은우.jpg"
         alt="Next.js logo"
           width={333}
@@ -16,7 +15,7 @@ export default function Home() {
           <li className="mb-2 tracking-[-.01em]">
             안녕{""}
              <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-             부트캠프 장미랑 입니다 
+             부트캠프  입니다 
              </code>
             .
           </li>
