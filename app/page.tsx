ee6fddb8a57823341c92615c4c1c389a19a87030
vpdtn0604 <vpdtn0604@gmail.com>
2025-09-24 17:12:30 +0900

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,36 @@
 import Image from "next/image";
 
-export default function Home() {
+async function getResumeInfo() {
+  const res = await fetch('https://raw.githubusercontent.com/startuser0604/first-deploy/refs/heads/main/service/resume_general_info_service.json');
+  // API 응답이 성공적인지 확인
+  if (!res.ok) {
+    // 응답이 실패하면 오류를 던져 Next.js가 오류 페이지를 보여주도록 함
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+}
+export default async function Home() {
+  const data = await getResumeInfo();
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    
+    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="/차은우.jpg"
           alt="차은우 사진"
-
           width={333}
           height={38}
           priority
         />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
+
+	      <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
 
-            안녕{""}
+          안녕{""}
              <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-             부트캠프  입니다 
+	          {data.name}  입니다. 
              </code>
             .
           </li>
@@ -103,3 +115,5 @@ export default function Home() {
     </div>
   );
 }
+
+
